593b318c1dee89af2d21d47df4dcb44965c0e172
Sasha Zbrozek <s.zbrozek@gmail.com>
2018-07-05 17:25:09 -0700

    Plotting improvements
    - Added background image.
    - Plotted actual sample points.
    - Plotted interpolated points.
    - Placed data on real coordinates.
    - Switched from heat map to contour plot.

--- g81-to-heatmap.py
+++ g81-to-heatmap.py
@@ -1,6 +1,8 @@
+import datetime
 import numpy as np
 import re
 import matplotlib
+import matplotlib.image as mpimg
 
 # Tell matplotlib to not worry about DISPLAY
 matplotlib.use('Agg')
@@ -38,40 +40,60 @@ g81_list_of_lists = []
 for line in g81_output_parsed.splitlines():
     g81_list_of_lists.append([float(i) for i in line.split(",")])
 
-# Let's take our list of lists and make it into
-# a numpy array that matplotlib can do something
-# with. We'll also take this opportunity to
-# reverse the order (Y Axis). Of course, this will
-# make things upside down for the user. So, we'll
-# flip it again before rendering the heatmap image.
-# This lets us get 0,0 at the bottom left corner.
-g81_array = np.array(list(reversed(g81_list_of_lists)))
+g81_xyz_list_of_lists = []
+row_count = 0
+col_count = 0
+x_size = 250
+y_size = 210
+x_probe_offset = 23
+y_probe_offset = 5
+pinda_diameter = 8
+left_probe_bed_position = 13.0 + x_probe_offset
+right_probe_bed_position = 216.0 + x_probe_offset
+back_probe_bed_position = 204.4 + y_probe_offset - pinda_diameter/2
+front_probe_bed_position = 8.4 + y_probe_offset - pinda_diameter/2
+x_inc = (right_probe_bed_position - left_probe_bed_position) / 6 
+y_inc = (back_probe_bed_position - front_probe_bed_position) / 6
+x_vals = np.zeros(7)
+y_vals = np.zeros(7)
+z_vals = np.zeros(shape=(7,7))
+for col in g81_list_of_lists:
+  for val in col:
+    x_vals[col_count] = col_count*x_inc + left_probe_bed_position
+    y_vals[row_count] = row_count*y_inc + front_probe_bed_position
+    z_vals[col_count][row_count] = val
+    row_count = row_count + 1
+  col_count = col_count + 1
+  row_count = 0
 
 # Set figure and gca objects, this will let us
 # adjust things about our heatmap image as well
 # as adjust axes label locations.
-fig = plt.figure()
+fig = plt.figure(dpi=96, figsize=(12, 9))
 ax = plt.gca()
 
-# Calculate our heatmap. Interpolation is used
-# to create the smooth looks. At this point you
-# can still adjust some visual elements later.
-# "cmap" controls the matplotlib colormap scheme.
-plt.imshow(g81_array, interpolation='spline16', cmap='plasma')
+for x in x_vals:
+  for y in y_vals:
+    plt.plot(x, y, '.', color='k')
+for x in [0, 3, 6]:
+  for y in [0, 3, 6]:
+    plt.plot(x_vals[x], y_vals[y], 'o', color='m')
+
+plt.contourf(x_vals, y_vals[::-1], z_vals, alpha=.5, antialiased=True)
+img = mpimg.imread('mk42_full-pcb.png')
+plt.imshow(img, extent=[x_vals[0]-37, x_vals[6]+14, y_vals[0]-16.4, y_vals[6]+37], interpolation="lanczos")
+ax.set_xlim(left=0, right=x_size)
+ax.set_ylim(bottom=0, top=y_size)
 
 # Set various options about the graph image before
 # we generate it. Things like labeling the axes and
 # colorbar, and setting the X axis label/ticks to
 # the top to better match the G81 output.
-plt.title("Mesh Level Correction Results")
-plt.xlabel("X Axis")
-plt.ylabel("Y Axis")
-plt.colorbar(label="Bed Variance (in mm)")
-
-# Flip that Y Axis again to put 0 at the bottom.
-# Since we inverted our Y Axis above as well, this
-# will also correct the view on the final heatmap.
-ax.invert_yaxis()
+plt.title("Mesh Level: " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+plt.axis('image')
+ax.axes.get_xaxis().set_visible(False)
+ax.axes.get_yaxis().set_visible(False)
+plt.colorbar(label="Bed Variance: " + str(round(z_vals.max() - z_vals.min(), 3)) + "mm")
 
 # Save our graph as an image in the current directory.
-fig.savefig('g81_heatmap.png')
+fig.savefig('g81_heatmap.png', bbox_inches="tight")
